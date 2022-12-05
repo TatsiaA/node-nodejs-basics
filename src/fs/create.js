e10964637ac64014e4bdce6c23805848908df5d8
TatsiaA <taniokk@mail.ru>
2022-12-05 22:14:02 +0300

    create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
+import * as url from 'url';
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const create = async () => {
-    // Write your code here 
+    fs.access(path.join(dirname, 'files/fresh.txt'), fs.constants.F_OK).then(() => {
+        console.error('FS operation failed');
+    }).catch(() => {
+        fs.writeFile(
+            path.join(dirname, 'files/fresh.txt'),
+            'I am fresh and young',
+            (err) => {
+                if (err) throw err;
+            }
+        )
+    });
 };
 
 await create();
