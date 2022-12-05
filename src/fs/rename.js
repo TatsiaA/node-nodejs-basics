de5c30e881b567e427afe445e3a051ecc8070cdc
TatsiaA <taniokk@mail.ru>
2022-12-06 01:42:04 +0300

    rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import * as url from 'url';
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const oldFile = dirname + '/files/wrongFilename.txt';
+const newFile = dirname + '/files/properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+    fs.access(oldFile, fs.constants.F_OK).then(() => {
+        fs.access(newFile, fs.constants.F_OK).then(() => {
+            console.error('FS operation failed');
+        }).catch(() => {
+            fs.rename(oldFile, newFile);
+        })
+    }).catch(err => console.error('FS operation failed'))
 };
 
 await rename();
